c038b868006fe68a66e9582840377cdf3d591b77
tmeits <tmeits@gmail.com>
2015-06-02 15:14:50 +0600

    add Rastrigin function

--- nminimize.py
+++ nminimize.py
@@ -5,4 +5,9 @@ import pandas
 
 match = lambda a, b: [ b.index(x)+1 if x in b else None for x in a ]
 
-
+def NRFunc(x):
+    """"
+    In mathematical optimization, the Rastrigin function is a non-convex function used as a performance test problem for optimization algorithms.
+    """"
+    A=np.array(x)
+    return sum(A^2-10.*np.cos(2.*n(pi)*A))+20.
